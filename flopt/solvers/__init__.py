12a66f4b326b2f4997291cd3c6246118209ff5e7
立岩斉明 <n-tateiwa@math.kyushu-u.ac.jp>
2020-05-10 18:09:07 +0900

    add ScipySearch

--- flopt/solvers/__init__.py
+++ flopt/solvers/__init__.py
@@ -1,15 +1,16 @@
-from .sequential_update_searches import(
+from flopt.solvers.sequential_update_searches import(
     SequentialUpdateSearch,
     RandomSearch,
     TwoOpt
 )
-from .optuna_searches import (
+from flopt.solvers.optuna_searches import (
     OptunaSearch,
     OptunaTPESearch,
     OptunaCmaEsSearch
 )
-from .hyperopt_search import HyperoptTPESearch
-from .swarm_intelligence_searches import ShuffledFrogLeapingSearch
+from flopt.solvers.hyperopt_search import HyperoptTPESearch
+from flopt.solvers.swarm_intelligence_searches import ShuffledFrogLeapingSearch
+from flopt.solvers.scipy_search import ScipySearch
 
 algos = {
     'RandomSearch'     : RandomSearch,
@@ -17,7 +18,8 @@ algos = {
     'OptunaTPESearch'  : OptunaTPESearch,
     'OptunaCmaEsSearch': OptunaCmaEsSearch,
     'HyperoptTPESearch': HyperoptTPESearch,
-    'SFLA'             : ShuffledFrogLeapingSearch
+    'SFLA'             : ShuffledFrogLeapingSearch,
+    'ScipySearch'      : ScipySearch,
 }
 
 def Solver(algo='RandomSearch'):
