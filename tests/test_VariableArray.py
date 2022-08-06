020ea2401fa0f91947ac1b1926e2942f2382bf49
nariaki3551 <nariaki3551@gmail.com>
2022-08-06 17:06:17 +0900

    add test

--- tests/test_VariableArray.py
+++ tests/test_VariableArray.py
@@ -23,3 +23,10 @@ def test_VariableArrayWithDict2():
     assert va.index(y.toMonomial()) == 1
     assert va.index(z.toMonomial()) == 2
 
+def test_VariableArrayWithDict2():
+    x = Variable.array('x', (2, 2), cat='Continuous')
+    assert x.index(x[0, 0].toMonomial()) == (0, 0)
+    assert x.index(x[0, 1].toMonomial()) == (0, 1)
+    assert x.index(x[1, 0].toMonomial()) == (1, 0)
+    assert x.index(x[1, 1].toMonomial()) == (1, 1)
+
