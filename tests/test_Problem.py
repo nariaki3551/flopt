95fd7854db869c59d3f38168e2e5100eccdcff59
nariaki3551 <nariaki3551@gmail.com>
2023-06-04 11:56:58 +0900

    fix bug

--- tests/test_Problem.py
+++ tests/test_Problem.py
@@ -110,6 +110,43 @@ def test_Problem_getSolution():
         )
 
 
+def test_Problem_getSolution_AutoSearch():
+    # Variables
+    a = Variable("a", lowBound=0, upBound=1, cat="Integer")
+    b = Variable("b", lowBound=1, upBound=2, cat="Continuous")
+    c = Variable("c", lowBound=1, upBound=3, cat="Continuous")
+
+    # Problem
+    prob = Problem(name="Test")
+    x = np.array([a, b, c], dtype=object)
+    J = np.array([[1, 2, 1], [0, 1, 1], [0, 0, 3]])
+    h = np.array([1, 2, 0])
+    prob += -(x.T).dot(J).dot(x) - (h.T).dot(x)
+
+    # Solver
+    solver = Solver("auto")
+    solver.setParams(max_k=2, timelimit=1)  # set max_k > 1
+
+    # solve
+    prob.solve(solver, msg=True)
+
+    from itertools import count
+
+    # get k-top solutions
+    for k in count(1):
+        try:
+            solution = prob.getSolution(k=k)
+            prob.setSolution(k=k)
+        except IndexError:
+            break
+        var_dict = solution.toDict()
+        print(
+            f"{k:2d}-th obj = {prob.obj.value():.4f}",
+            {name: f"{var.value():.4f}" for name, var in var_dict.items()},
+        )
+
+
+
 def test_Problem_duplicate_constraint():
     # Variables
     a = Variable("a", lowBound=0, upBound=1, cat="Integer")
